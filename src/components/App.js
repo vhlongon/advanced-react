4d0748270de2a9d3f1adf23ee936c6a5dce39386
Victor Longon <vhlongon@yahoo.com.br>
2017-06-11 17:52:15 +0200

    add tests to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <h1>{this.props.title}</h1>
       </div>
     );
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, render } from 'enzyme';
 import App from '../App';
+import Header from '../Header';
 
 describe('App', () => {
-  it('todo', () => {
+  it('should render a Header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<Header />)).toBe(true);
   });
 });
