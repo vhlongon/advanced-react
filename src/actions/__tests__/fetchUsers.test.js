9639daf8b24911a641fe0c74cc6ed780db6d1417
Victor Longon <vhlongon@yahoo.com.br>
2017-06-21 20:46:37 +0200

    add test to async action

--- src/actions/__tests__/fetchUsers.test.js
+++ src/actions/__tests__/fetchUsers.test.js
@@ -1,14 +1,47 @@
 import { FETCH_USERS } from '../types';
 import { fetchUsers } from '../index';
+import moxios from 'moxios';
 
 describe('fetchUsers', () => {
-  it('should return FETCH_USERS action', () => {
-    //   const action = {
-    //     type: FETCH_USERS,
-    //     payload: []
-    //   };
-    //   expect(fetchUsers([])).toEqual(action);
-    // TODO - test async action
-    expect(true).toBe(true);
+  beforeEach(function() {
+    // import and pass your custom axios instance to this method
+    moxios.install();
+  });
+  afterEach(function() {
+    // import and pass your custom axios instance to this method
+    moxios.uninstall();
+  });
+
+  it('should return FETCH_USERS action with the correct payload', () => {
+    const users = [
+      {
+        name: 'Deedee',
+        company: 'Ramones',
+        email: 'deedee@ramones.com',
+        id: 1
+      },
+      { name: 'Joey', company: 'Ramones', email: 'joey@ramones.com', id: 2 },
+      {
+        name: 'Jhonny',
+        company: 'Ramones',
+        email: 'jhonny@ramones.com',
+        id: 3
+      },
+      { name: 'Marc', company: 'Ramones', email: 'marc@ramones.com', id: 4 }
+    ];
+    const action = {
+      type: FETCH_USERS,
+      payload: { data: users }
+    };
+    moxios.wait(() => {
+      moxios.requests.mostRecent()
+        .respondWith({
+          status: 200,
+          response: {
+            data: users
+          }
+        })
+        .then(() => expect(fetchUsers()).toEqual(action));
+    });
   });
 });
