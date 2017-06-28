34d7ec6e48d950bb113d087ffb581046e1ac3158
Victor Longon <vhlongon@yahoo.com.br>
2017-06-28 20:02:52 +0200

    add guard to prevent wrong data to be post to users table

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -2,6 +2,10 @@ const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
   const { body: { email, password } } = req;
+
+  if(!email || !password) {
+    return res.status(422).send({error: 'You must provide email and password'})
+  }
   // check if a user with the given email exists
   User.findOne({ email }).exec().then(
     (existingUser, next) => {
