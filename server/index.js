b1da2c60b90d965082001ea751f9c5993b8586c7
Victor Longon <vhlongon@yahoo.com.br>
2017-06-28 18:39:56 +0200

    add server router

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 // App setup
 // morgan is a logging middleware to ouput each request from the browser in the terminal
 app.use(morgan('combined'));
 // bodyParser is a middleware responsive for parsing each incoming request as json
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
