2ebe1c6866af22c5635d4435309b0a891222939d
Victor Longon <vhlongon@yahoo.com.br>
2017-06-28 18:28:44 +0200

    add nodemon, and start scafolding the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
 // main starter point of the application on the server side
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App setup
+// morgan is a logging middleware to ouput each request from the browser in the terminal
+app.use(morgan('combined'));
+// bodyParser is a middleware responsive for parsing each incoming request as json
+app.use(bodyParser.json({ type: '*/*' }));
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log(`Serve listning on: ${port}`);
--- src/components/App.js
+++ src/components/App.js
@@ -8,5 +8,4 @@ export const App = ({ title }) => {
   );
 };
 
-
 export default App;
