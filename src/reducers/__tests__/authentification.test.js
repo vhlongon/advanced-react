ac8a124cc070aa69030eb3cdf2e0bf06371d7455
Victor Hugo Longon <victor.longon@etraveli.com>
2017-08-02 16:13:45 +0200

    update initial state on authentification reducer tests to match

--- src/reducers/__tests__/authentification.test.js
+++ src/reducers/__tests__/authentification.test.js
@@ -10,16 +10,20 @@ import deepFreeze from 'deep-freeze';
 
 describe('authentication', () => {
   it('returns the initial state', () => {
-    const initialState = { isAuthenticated: false, payload: null };
+    const initialState = { isAuthenticated: false, payload: null, error: null };
     deepFreeze(initialState);
     expect(authReducer(initialState, {})).toEqual(initialState);
   });
 
   describe('when the signin request is good', () => {
     it('handles action with type SIGNIN_SUCCESS', () => {
-      const initialState = { isAuthenticated: false, error: null };
+      const initialState = {
+        isAuthenticated: false,
+        payload: null,
+        error: null
+      };
       const action = signinSuccess();
-      const newState = { isAuthenticated: true, error: null };
+      const newState = { isAuthenticated: true, error: null, payload: null };
       deepFreeze(initialState);
 
       expect(authReducer(initialState, action)).toEqual(newState);
@@ -29,9 +33,13 @@ describe('authentication', () => {
   describe('when the signin request is bad', () => {
     it('handles action with type SIGNIN_FAILURE', () => {
       const error = 'error';
-      const initialState = { isAuthenticated: false, error: null };
+      const initialState = {
+        isAuthenticated: false,
+        payload: null,
+        error: null
+      };
       const action = signinFailure(error);
-      const newState = { isAuthenticated: false, error };
+      const newState = { isAuthenticated: false, error, payload: null };
       deepFreeze(initialState);
 
       expect(authReducer(initialState, action)).toEqual(newState);
@@ -40,9 +48,13 @@ describe('authentication', () => {
 
   describe('when the signup request is good', () => {
     it('handles action with type SIGNUP_SUCCESS', () => {
-      const initialState = { isAuthenticated: false, error: null };
+      const initialState = {
+        isAuthenticated: false,
+        payload: null,
+        error: null
+      };
       const action = signupSuccess();
-      const newState = { isAuthenticated: true, error: null };
+      const newState = { isAuthenticated: true, error: null, payload: null };
       deepFreeze(initialState);
 
       expect(authReducer(initialState, action)).toEqual(newState);
@@ -52,9 +64,13 @@ describe('authentication', () => {
   describe('when the signup request is bad', () => {
     it('handles action with type SIGNUP_FAILURE', () => {
       const error = 'error';
-      const initialState = { isAuthenticated: false, error: null };
+      const initialState = {
+        isAuthenticated: false,
+        payload: null,
+        error: null
+      };
       const action = signupFailure(error);
-      const newState = { isAuthenticated: false, error };
+      const newState = { isAuthenticated: false, error, payload: null };
       deepFreeze(initialState);
 
       expect(authReducer(initialState, action)).toEqual(newState);
@@ -62,9 +78,9 @@ describe('authentication', () => {
   });
 
   it('handles action with type SIGNOUT', () => {
-    const initialState = { isAuthenticated: undefined };
+    const initialState = { isAuthenticated: false, payload: null, error: null };
     const action = signout();
-    const newState = { isAuthenticated: false, error: null };
+    const newState = { isAuthenticated: false, error: null, payload: null };
     deepFreeze(initialState);
 
     expect(authReducer(initialState, action)).toEqual(newState);
